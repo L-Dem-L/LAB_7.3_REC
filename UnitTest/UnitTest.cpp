c133e0806d205eef3ce0466f08b78899c137e5bb
Demian Rymar <ua.demyan@gmail.com>
2024-02-29 20:59:34 +0200

    Fixed

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -9,7 +9,49 @@ namespace UnitTest
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		
+		TEST_METHOD(TestFirstZeroColumn)
+		{
+			const int rowCount = 3;
+			const int colCount = 3;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+
+
+			a[0][0] = 1; a[0][1] = 0; a[0][2] = 3;
+			a[1][0] = 4; a[1][1] = 5; a[1][2] = 6;
+			a[2][0] = 7; a[2][1] = 8; a[2][2] = 9;
+
+			int result = firstZeroColumn(a, rowCount, colCount, 0, 0, -1);
+
+			Assert::AreEqual(1, result);
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+
+		TEST_METHOD(TestRowSum)
+		{
+			const int rowCount = 3;
+			const int colCount = 3;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+
+
+			a[0][0] = -2; a[0][1] = 4; a[0][2] = -6;
+			a[1][0] = 1; a[1][1] = 3; a[1][2] = 5;
+			a[2][0] = -8; a[2][1] = 6; a[2][2] = -4;
+
+			int result = rowSum(a, 0, colCount, 0, 0);
+
+			Assert::AreEqual(-8, result);
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] a[i];
+			delete[] a;
+		}
 		TEST_METHOD(TestChangeRows)
 		{
             const int rowCount = 3;
